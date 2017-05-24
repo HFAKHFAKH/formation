46111da63cfeaeb973cc4d334e3cb86b4fc2f847
hfakhfakh <houcem.fakhfakh@groupe-telnet.net>
2017-05-24 15:41:23 +0100

    add unit test

--- src/app/formation/formation-form/formation-form.component.spec.ts
+++ src/app/formation/formation-form/formation-form.component.spec.ts
@@ -1,3 +1,11 @@
+import { FormationListContainerComponent } from './../formation-list-container/formation-list-container.component';
+import { FormationContainerComponent } from './../formation-container/formation-container.component';
+import { FormationDetailsComponent } from './../formation-details/formation-details.component';
+import { FormationListComponent } from './../formation-list/formation-list.component';
+import { SharedModule } from './../../shared/shared.module';
+import { FormationRoutingModule } from './../formation-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormationFormComponent } from './formation-form.component';
@@ -8,9 +16,21 @@ describe('FormationFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormationFormComponent ]
-    })
-    .compileComponents();
+      imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FormationRoutingModule,
+        SharedModule
+      ],
+      declarations: [
+        FormationListComponent,
+        FormationDetailsComponent,
+        FormationFormComponent,
+        FormationContainerComponent,
+        FormationListContainerComponent
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/shared/services/formation.service.spec.ts
+++ src/app/shared/services/formation.service.spec.ts
@@ -1,15 +1,76 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { Subscription } from 'rxjs/Subscription';
+import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
 
 import { FormationService } from './formation.service';
+
+
 
 describe('FormationService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FormationService]
+      imports: [HttpModule],
+      providers: [
+        FormationService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
     });
   });
+
+  it('should construst', inject([FormationService, MockBackend], (service, mockBackend) => {
+    expect(service).toBeDefined();
+  }));
 
   it('should be created', inject([FormationService], (service: FormationService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('fetch', () => {
+    const mockResponse = [{
+      title: 'title1',
+      description: 'description'
+    }];
+
+    it('should parse response', async(inject(
+      [FormationService, MockBackend], (service, mockBackend) => {
+
+        mockBackend.connections.subscribe(conn => {
+          conn.mockRespond(new Response(new ResponseOptions({ body: JSON.stringify(mockResponse) })));
+        });
+
+        const result = service.fetch();
+
+        result.subscribe(res => {
+          expect(res).toEqual([{
+            title: 'title',
+            description: 'description'
+          }]);
+        });
+      })));
+  });
+
+
+  it('should be save formation', inject([FormationService], (service: FormationService) => {
+    expect(service.fetch()).toBeTruthy();
+  }));
+
+  it('should be find formation', inject([FormationService], (service: FormationService) => {
+    expect(service.fetch()).toBeTruthy();
+  }));
+
+  it('should be remove formation', inject([FormationService], (service: FormationService) => {
+    expect(service.fetch()).toBeTruthy();
+  }));
+
+  it('should be edit formation', inject([FormationService], (service: FormationService) => {
+    expect(service.fetch()).toBeTruthy();
+  }));
+
 });
